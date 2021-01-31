744043c83da30ebfea9c27efa1871d9e3916e5c9
Matt Beiswenger <mattbeis@yahoo.com>
2021-01-30 20:51:07 -0600

    Format links and print to console

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+#!/usr/bin/env node
+
 const links = [
   { title: 'Twitter', url: 'beis.io/twitter' },
   { title: 'Instagram', url: 'beis.io/insta' },
 ]
+
+const longestTitleLength = links
+  .map((link) => link.title)
+  .reduce((longest, current) => {
+    return longest.length > current.length ? longest : current.length
+  }, 0)
+
+links.forEach((link) => {
+  console.log(
+    `${link.title.padEnd(longestTitleLength, ' ')} -> https://${link.url}`
+  )
+})
